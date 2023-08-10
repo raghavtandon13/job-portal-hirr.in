e32e387048a78f67d9ae32b2dbc4edd7b3900e49
Raghav <raghav.tandon13@gmail.com>
2023-08-10 23:53:26 +0530

    changes

--- client/src/components/searchResults.jsx
+++ client/src/components/searchResults.jsx
@@ -1,25 +1,24 @@
 // SearchResults.jsx
 
-import React, { useState, useEffect } from 'react';
-import Card from './card'; // Import the Card component
+import React, { useState, useEffect } from "react";
+import Card from "./card"; // Import the Card component
 
 const SearchResults = () => {
   const [responseData, setResponseData] = useState([]);
 
   useEffect(() => {
-    // Function to fetch data from the backend API and set the response in state
     const fetchDataFromBackend = async () => {
       try {
-        const response = await fetch('/your-backend-api-url');
+        const response = await fetch("/your-backend-api-url");
         const data = await response.json();
         setResponseData(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchDataFromBackend();
-  }, []); // Run the effect only once on component mount
+  }, []);
 
   return (
     <div className="search-results">
