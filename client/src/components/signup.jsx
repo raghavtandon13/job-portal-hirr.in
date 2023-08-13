011bfd9e6d9590d5385ab73b35c13b1e0a5f5817
Raghav <raghav.tandon13@gmail.com>
2023-08-13 14:43:56 +0530

    changes

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -6,50 +6,22 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [isFileSelected, setIsFileSelected] = useState(false);
+
   const navigate = useNavigate();
   const handleReset = () => {
     document.getElementById("name").value = "";
     document.getElementById("phone").value = "";
     document.getElementById("email").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("pin").value = "";
     document.getElementById("password").value = "";
-    document.getElementById("re-password").value = "";
   };
 
   const [profilePicture, setProfilePicture] = useState(null);
 
   const handleProfilePictureChange = (file) => {
     setProfilePicture(file);
+    setIsFileSelected(true);
   };
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const data = { name, email, password };
-  //   try {
-  //     const response = await fetch("http://localhost:3000/signup/user/", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(data),
-  //     });
-
-  //     if (response.ok) {
-  //       const responseData = await response.json();
-  //       const token = responseData.token;
-
-  //       console.log("Signup successful!");
-  //       localStorage.setItem("token", token);
-  //       console.log("token stored");
-  //       navigate("/home");
-  //     } else {
-  //       console.error("Signup failed");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   }
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -127,22 +99,26 @@ const Signup = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <input
-            type="password"
-            name="password"
-            id="re-password"
-            placeholder="Re-enter Password"
-          />
-          <input
-            type="file"
-            name="profilePicture"
-            accept="image/*"
-            onChange={(e) => handleProfilePictureChange(e.target.files[0])}
-          />
+          <div className="file-input-container">
+            <input
+              type="file"
+              name="profilePicture"
+              accept="image/*"
+              id="profilePictureInput"
+              onChange={(e) => handleProfilePictureChange(e.target.files[0])}
+              title="Upload profile picture"
+              required
+            />
+            {isFileSelected && (
+              <span className="file-selected-icon">&#10003;</span>
+            )}
+          </div>
         </div>
         <div className="signup-button">
           <button type="submit">Submit</button>
-          <button onClick={handleReset}>Reset</button>
+          <button type="button" onClick={handleReset}>
+            Reset
+          </button>
         </div>
       </form>
     </div>
