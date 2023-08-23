aae74340f036a557b9357dad1ff18c9ecb92d29f
Raghav <raghav.tandon13@gmail.com>
2023-08-23 14:56:21 +0530

    done

--- client/src/pages/Searchpage.jsx
+++ client/src/pages/Searchpage.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Search from "../components/search";
-import SearchResults from "../components/searchResults"
+import SearchResults from "../components/searchResults";
 import Footer from "../components/Footer";
 import "./Searchpage.css";
 
 const Searchpage = () => {
   return (
     <>
-      <Navbar
-        buttonLink="/login"
-        buttonLabel="Login"
-        button2Link="/signup"
-        button2Label="Register"
-      />
-      <div className="wrapper-searchpage">
-        <Search />
-        <SearchResults/>
+      <div className="container">
+        <Navbar
+          buttonLink="/login"
+          buttonLabel="Login"
+          button2Link="/signup"
+          button2Label="Register"
+        />
+        <div className="wrapper-searchpage">
+          <Search />
+          <SearchResults />
+        </div>
       </div>
-      {/* <Footer /> */}
+        <Footer />
     </>
   );
 };
