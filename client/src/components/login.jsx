a99b720af39b360553dc465b79525ce75cd98be7
Raghav Tandon <121853010+raghavtandon13@users.noreply.github.com>
2024-01-21 18:34:09 +0530

    changes in dev block in login.jsx 2

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
         ////////////////////////////////////////////
         ////////////////////////////////////////////
 
-        window.location.reload();
+        //window.location.reload();
 
         navigate("/home");
       } else {
