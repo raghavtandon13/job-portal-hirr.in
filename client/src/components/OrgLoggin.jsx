b34cbf544fd0f0ed21ba7952d97aeb2d25bbcdc2
Raghav <raghav.tandon13@gmail.com>
2024-01-21 13:46:24 +0530

    OrgLoggin

--- client/src/components/OrgLoggin.jsx
+++ client/src/components/OrgLoggin.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import "./Orgloggin.css";
+import "./OrgLoggin.css";
 
 const OrgLogin = () => {
   const [email, setEmail] = useState("");
